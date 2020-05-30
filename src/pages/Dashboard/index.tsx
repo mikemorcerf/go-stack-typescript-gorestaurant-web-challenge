d15ecfa7e7bdd9be80e23f6d6afd0fa1b7930646
Michael Moura <mikemorcerf@gmail.com>
2020-05-30 17:11:58 -0400

    pass test to delete dish off Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -91,7 +91,13 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    const foodsUpdated = foods.filter(
+      foodOnDashboard => foodOnDashboard.id !== id,
+    );
+
+    setFoods(foodsUpdated);
   }
 
   function toggleModal(): void {
